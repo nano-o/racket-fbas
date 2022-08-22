830ff4c0551065e3c98f5b8eb2d858e02197b46f
Giuliano Losa <giuliano@losa.fr>
2022-08-22 15:10:57 -0700

    move qset->fbas to fba.rkt

--- fba.rkt
+++ fba.rkt
@@ -10,16 +10,110 @@
 (define (zip l1 l2)
   (map cons l1 l2))
 
-; an FBAS is a map from nodes to sets of slices
-; TODO: check there are no duplicate slices, at least one slice, etc.?
+; a node is something for which eqv? is semantic equivalence, i.e. interned symbols, numbers, and characters.
+(define node/c
+  (or/c (and/c symbol? symbol-interned?) number? char?))
+
+; A FBAS is a map from nodes to sets of slices
 (define fbas/c
-  (and
+  (and/c
     immutable?
-    (hash/c number? (*list/c (*list/c number?)))))
+    (hash/c node/c (listof (listof node/c)))))
+
+; A quorum-set represents sets of sets of nodes using thresholds
+; This is what is used on the wire in the Stellar network
+(define-struct qset (threshold validators inner-qsets))
+
+; The quorum-set generation tool included in core uses validators array
+(struct validator-description (quality home-domain))
+(define validatos-array/c
+  (hash/c any/c validator-description?))
+
+; We will use cartesian-product to transform a qset into an fbas
+(define/contract (cartesian-product ss)
+  (-> list? list?)
+  (match ss
+    [(list s1) s1]
+    [(list s1 s2)
+     (apply
+       append
+       (for/list ([e1 s1])
+         (for/list ([e2 s2])
+           (list e1 e2))))]
+    [(cons s1 ss)
+     (define ts (cartesian-product ss))
+     (apply
+       append
+       (for/list ([e s1])
+         (for/list ([t ts])
+           (cons e t))))]))
+
+(module+ test
+  (require rackunit)
+  (test-case
+    "cartesian-product"
+    (check-equal?
+      (cartesian-product '((a b)))
+      '(a b))
+    (check-equal?
+      (cartesian-product '((a b) ()))
+      empty)
+    (check-equal?
+      (cartesian-product '((a b) (1 2)))
+      '((a 1) (a 2) (b 1) (b 2)))
+    (check-equal?
+      (cartesian-product '((a b) (1 2) (x y)))
+      '((a 1 x) (a 1 y) (a 2 x) (a 2 y) (b 1 x) (b 1 y) (b 2 x) (b 2 y)))))
+
+(define (expand-qset qs)
+  (define es
+    (append (hash-ref qs 'innerQuorumSets) (hash-ref qs 'validators)))
+  (define es-refined
+    ; list of sets of sets
+    (for/list ([e es])
+      (cond
+        [(hash? e) (expand-qset e)]
+        [else (list (list e))])))
+  (define cs
+    (combinations es-refined (hash-ref qs 'threshold)))
+  (apply
+    append
+    (for/list ([c cs])
+      (for/list ([t (cartesian-product c)])
+        (apply append t)))))
 
 (module+ test
   (require rackunit)
   (provide (all-defined-out))
+
+  (define qset-1
+    (hash 'threshold 2 'validators '(1 2 3) 'innerQuorumSets empty))
+  (define qset-2
+    (hash 'threshold 2 'validators empty 'innerQuorumSets empty))
+  (define qset-3
+    (hash 'threshold 2 'validators '(a b c) 'innerQuorumSets empty))
+  (define qset-4
+    (hash 'threshold 2 'validators '(x y z) 'innerQuorumSets empty))
+  (define qset-5
+    (hash 'threshold 2 'validators empty 'innerQuorumSets (list qset-1 qset-3 qset-4)))
+  (define qset-6
+    (hash 'threshold 2 'validators (list 'A) 'innerQuorumSets (list qset-1 qset-3 qset-4)))
+  (test-case
+    "quorumSet->slices"
+    (check-equal?
+      (expand-qset qset-1)
+      '((1 2) (1 3) (2 3)))
+    (check-equal?
+      (expand-qset qset-2)
+      empty))
+    (check-equal?
+      (expand-qset qset-5)
+      '((1 2 a b) (1 2 a c) (1 2 b c) (1 3 a b) (1 3 a c) (1 3 b c) (2 3 a b) (2 3 a c) (2 3 b c) (1 2 x y) (1 2 x z) (1 2 y z) (1 3 x y) (1 3 x z) (1 3 y z) (2 3 x y) (2 3 x z) (2 3 y z) (a b x y) (a b x z) (a b y z) (a c x y) (a c x z) (a c y z) (b c x y) (b c x z) (b c y z)))
+    (check-equal?
+      (expand-qset qset-6)
+      '((1 2 a b) (1 2 a c) (1 2 b c) (1 3 a b) (1 3 a c) (1 3 b c) (2 3 a b) (2 3 a c) (2 3 b c) (1 2 x y) (1 2 x z) (1 2 y z) (1 3 x y) (1 3 x z) (1 3 y z) (2 3 x y) (2 3 x z) (2 3 y z) (1 2 A) (1 3 A) (2 3 A) (a b x y) (a b x z) (a b y z) (a c x y) (a c x z) (a c y z) (b c x y) (b c x z) (b c y z) (a b A) (a c A) (b c A) (x y A) (x z A) (y z A))))
+
+(module+ test
 
   (define (threshold-fbas t n)
     fbas/c
@@ -32,8 +126,7 @@
   (define fbas-1
     (threshold-fbas 3 4))
 
-  (define/contract fbas-2
-    fbas/c
+  (define fbas-2
     (let ([slices
             (combinations '(1 2 3) 2)])
       (hash-set fbas-1 1 slices)))
@@ -46,7 +139,7 @@
       (hash-set (hash-set (threshold-fbas 4 5) 1 slices-1) 5 slices-2))))
 
 (define/contract (has-slice-in? fbas p Q)
-  (-> fbas/c number? list? boolean?)
+  (-> fbas/c node/c list? boolean?)
   (define slices
     (hash-ref fbas p))
   (for/or ([s slices])
@@ -58,7 +151,7 @@
       (has-slice-in? fbas q Q)))
 
 (define (quorum-of? fbas p Q)
-  (-> fbas/c number? list? boolean?)
+  (-> fbas/c node/c list? boolean?)
   (and
     (has-slice-in? fbas p Q)
     (quorum? fbas Q)))
@@ -71,10 +164,10 @@
     (check-true (quorum-of? fbas-1 1 (list 2 3 4)))
     (check-false (quorum-of? fbas-1 1 (list 1 2)))))
 
-; TODO check whether the system is a topen
+; nomination protocol
 
 (define/contract (weight fbas p q)
-  (-> fbas/c number? number? number?)
+  (-> fbas/c node/c node/c number?)
   (define slices
     (invariant-assertion
       (λ (s) (not (empty? s)))
@@ -95,12 +188,11 @@
     (check-equal?
       (weight fbas-1 1 1) (/ 3 4))))
 
-; TODO add quorum sets and conversion to fbas
-
-; nomination protocol
+; TODO define a nomination module that uses this as interface (and attach a contract to it):
+(struct nomination-parameters (peers weight quorums))
 
 (define/contract (neighbors fbas N n)
-  (-> fbas/c (hash/c number? number?) number? list?)
+  (-> fbas/c (hash/c node/c number?) node/c list?)
   ; second parameter assign a number between 0 and 1 to each node
   (define peers
     (apply set-union (hash-ref fbas n)))
@@ -119,7 +211,7 @@
         '(1 2 3 4)))))
 
 (define/contract (leader fbas N P n)
-  (-> fbas/c (hash/c number? number?) (hash/c number? number?) number? number?)
+  (-> fbas/c (hash/c node/c number?) (hash/c node/c number?) node/c node/c)
   (define neighbors-weights
     (for/list
       ([m (neighbors fbas N n)])
@@ -143,7 +235,7 @@
       4)))
 
 (define/contract (nomination-step fbas N P s)
-  (-> fbas/c (hash/c number? number?) (hash/c number? number?) (hash/c number? number?) (hash/c number? number?))
+  (-> fbas/c (hash/c node/c number?) (hash/c node/c number?) (hash/c node/c node/c) (hash/c node/c node/c))
   (for/hash ([(n v) (in-hash s)])
     (cond
       [(not (equal? v -1)) (values n v)]
