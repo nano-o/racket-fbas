2c665c2f5056159e328ed9c673818d99892430d7
Giuliano Losa <giuliano@losa.fr>
2022-10-06 15:09:29 -0700

    work on qset.rkt

--- qset.rkt
+++ qset.rkt
@@ -1,17 +1,43 @@
 #lang scribble/lp2
 
-@title{Federated Byzantine Agreement Systems in racket}
+@title{Quorum sets}
 
 @section{Overview}
 
-This library consists of basic definitions, a function that computes a qset's
-corresponding slices, and tests.
+In this file, we define quorum sets and provide some utility functions, among
+which @code{quorum?}, which cheks whether a set is a quorum in a given system
+configuration.
 
 @chunk[<*>
 (require racket)
+<provide>
 <basic-qset-defs>
 <slices>
+<sat?>
+<system>
 <tests>]
+
+@chunk[<provide>
+(provide
+  node/c
+  conf/c
+  (contract-out
+    [struct qset
+      ((threshold exact-positive-integer?)
+       (validators (listof node/c))
+       (inner-qsets (listof qset?)))]
+    [qset-kw
+      (->
+        #:threshold exact-positive-integer?
+        #:validators (listof node/c)
+        #:inner (listof qset?)
+        qset?)]
+    [elems (-> qset? list?)]
+    [slices (-> qset? (set/c set?))]
+    [qset-member? (-> qset? node/c boolean?)]
+    [qset-members (-> qset? set?)]
+    [sat? (-> qset? set? boolean?)]
+    [quorum? (-> conf/c set? boolean?)]))]
 
 @section{Quorum sets}
 
@@ -19,7 +45,7 @@ corresponding slices, and tests.
 <node-contract>
 <qset-struct>
 <qset-keyword-constructor>
-<elems>]
+<qset-aux-functions>]
 
 A quorum set is a data structure used in practice by Stellar to describe the
 set of quorum slices of a node. In this file we provide functionality related
@@ -66,12 +92,49 @@ This allows to define qsets as follows.
   (define qset-6
     (qset-kw #:threshold 2 #:validators (list 'A) #:inner (list qset-1 qset-3 qset-4))))]
 
-Next we define a simple function @code{elems} that returns the concatenation of the
-validators and inner fields of a qset:
+Next we define a simple function @code{elems} that returns the concatenation of
+the validators and inner fields of a qset, a function @code{qset-empty?} that
+checks whether a qset is empty, @code{qset-mebmer?}, which checks whether a
+node appears anywhere in a qset, and @code{qset-members}, which returns all the
+nodes appearing anywhere in a qset.
 
-@chunk[<elems>
+@chunk[<qset-aux-functions>
+
 (define (elems qs)
-  (append (qset-validators qs) (qset-inner-qsets qs)))]
+  (append (qset-validators qs) (qset-inner-qsets qs)))
+
+(define (qset-empty? qs)
+  (empty? (elems qs)))
+
+(define (qset-member? qs p)
+  (define in-inner-qsets
+    (for/or ([q (qset-inner-qsets qs)])
+      (qset-member? q p)))
+  (define in-validators
+    (and (member p (qset-validators qs)) #t))
+  (or in-inner-qsets in-validators))
+
+(define (qset-members qs)
+  (apply
+    set-union
+    (cons
+      (list->set (qset-validators qs))
+      (for/list ([inn (qset-inner-qsets qs)])
+        (qset-members inn)))))]
+
+@chunk[<test-qset-members>
+(module+ test
+  (test-case
+    "qset-members"
+    (check-true (qset-member? qset-1 3))
+    (check-false (qset-member? qset-1 4))
+    (check-true (qset-member? qset-6 'z))
+    (check-equal?
+      (qset-members qset-1)
+      (set 1 2 3))
+    (check-equal?
+      (qset-members qset-6)
+      (set 'A 1 2 3 'a 'b 'c 'x 'y 'z))))]
 
 @subsection{From quorum sets to slices}
 
@@ -99,7 +162,7 @@ cartesian product of the corresponding sets of quorum slices.
         (for/list ([tuple (apply cartesian-product c)])
           (apply set-union tuple)))))
   (cond
-    [(empty? (elems qs))
+    [(qset-empty? qs)
      (set)]
     [else (ll->ss (slices-rec qs))]))]
 
@@ -133,6 +196,78 @@ Now a few tests:
       (slices qset-6)
       (ll->ss '((1 2 a b) (1 2 a c) (1 2 b c) (1 3 a b) (1 3 a c) (1 3 b c) (2 3 a b) (2 3 a c) (2 3 b c) (1 2 x y) (1 2 x z) (1 2 y z) (1 3 x y) (1 3 x z) (1 3 y z) (2 3 x y) (2 3 x z) (2 3 y z) (1 2 A) (1 3 A) (2 3 A) (a b x y) (a b x z) (a b y z) (a c x y) (a c x z) (a c y z) (b c x y) (b c x z) (b c y z) (a b A) (a c A) (b c A) (x y A) (x z A) (y z A))))))]
 
+@subsection{Satisfying a quorum set}
+
+A set of nodes satisfies a quorum set when it contains at least one of the
+quorum set's slices. The function @code{sat?} checks that a set of nodes
+@code{s} satisfies @code{qs} by checking that at least @code{k} elements of
+@code{qs}, where @code{k} is the threshold of @code{qs}, are recursively
+satisfied.
+
+@chunk[<sat?>
+(define (sat? qs s)
+  (define t
+    (for/fold
+      ([n 0])
+      ([e (elems qs)])
+      (cond
+        [(and (node/c e) (set-member? s e))
+         (+ n 1)]
+        [(and (qset? e) (sat? e s))
+         (+ n 1)]
+        [else n])))
+  (>= t (qset-threshold qs)))]
+
+@chunk[<test-sat?>
+(module+ test
+  (test-case
+    "sat?"
+    (check-true (sat? qset-1 (set 1 2)))
+    (check-false (sat? qset-1 (set 1)))
+    (check-true (sat? qset-5 (set 1 2 'x 'z)))
+    (check-true (sat? qset-6 (set 'A 'x 'z)))
+    (check-false (sat? qset-6 (set 'A 'a 'y)))))]
+
+We can also check that if @code{sat? qs s} then @code{s} contains a slice of @code{qs}:
+
+@chunk[<test-sat?-contains-slice>
+(module+ test
+  (test-case
+    "sat?-contains-slice"
+    (define nodes
+      (set->list (qset-members qset-6)))
+    (for ([s (map list->set (combinations nodes))])
+      (check-equal?
+        (sat? qset-6 s)
+        (for/or ([sl (slices qset-6)])
+          (subset? sl s))))))]
+
+@subsection{System configurations}
+
+A system configuration consists of a set of nodes and a quorum set for each
+node.
+
+@chunk[<conf/c>
+(define conf/c
+  (hash/c node/c qset?))]
+
+A set of nodes is a quorum when it is a non-empty set that satisfies the qsets
+of each of its members.
+
+@chunk[<quorum?>
+(define (quorum? conf q)
+  (and
+    (not (set-empty? q))
+    (for/and ([n q])
+      (sat? (hash-ref conf n) q))))]
+
+@chunk[<system>
+<conf/c>
+<quorum?>]
+
 @chunk[<tests>
 <test-qset-examples>
-<test-slices>]
+<test-qset-members>
+<test-slices>
+<test-sat?-contains-slice>
+<test-sat?>]
