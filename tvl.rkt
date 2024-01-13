343b605db621d7e10473c4fb386c5d2bb896762a
Giuliano Losa <giuliano@losa.fr>
2024-01-12 19:42:54 -0800

    remove truth tables from tvl.rkt

--- tvl.rkt
+++ tvl.rkt
@@ -1,150 +1,13 @@
-#lang sweet-exp racket/base
-(require rosette/safe
-         rosette/lib/synthax
-         syntax/parse/define)
-(require
-  (for-syntax
-    racket/syntax
-    syntax/to-string))
+; We use the rosette/safe language to enable symbolic execution
+; We use the sweet-exp adapter to enable infix syntax when we write logical formulas
+#lang sweet-exp rosette/safe
 
-(provide
-  bv-to-3
-  ∧ ∨ ¬ ⇒ ⊃ ⇔ ≡ ◇ □)
-
-;; We start by defining the truth tables
-;; Our logical values are 't, 'b, and 'f
-
-;; First we create a macro to make it easier to write down truth tables
-
-(begin-for-syntax
-  (define-syntax-class tvl-value
-    #:description "a three-valued logic value (either 't, 'b, or 'f)"
-    (pattern (~or* (~literal t) (~literal f) (~literal b)))))
-
-(define-syntax (define-truth-table stx)
-  (syntax-parse stx
-    [(_ operation:id
-        [value:tvl-value ... result:tvl-value] ...)
-     #:do [(define rows/datum
-             (for/list ([l (syntax->list #'((value ...) ...))])
-               (for/list ([e (syntax->list l)])
-                 (syntax->datum e))))
-           (define num-inputs
-             (length (car rows/datum)))
-           (define num-inputs-in-each-row
-             (for/and ([l rows/datum])
-               (equal? (length l) num-inputs)))
-           (define combinations
-             (apply cartesian-product
-                    (make-list num-inputs '(t b f))))
-           (define has-all-combinations
-             (set=? combinations rows/datum))]
-     #:fail-when (not num-inputs-in-each-row) "some rows have different sizes"
-     #:fail-when (not has-all-combinations) (format "missing combinations of inputs: ~a" (set-subtract combinations rows/datum))
-     (with-syntax
-         ([(arg ...)
-           (for/list ([i (in-range num-inputs)])
-             (format-id #'operation "arg-~a" i))])
-       #'(define (operation arg ...)
-         (case (list arg ...)
-           [((value ...)) (quote result)] ...
-           [else 'error])))]))
-
-;; Now we define the truth tables
-
-(define-truth-table ∧
-  [t t t]
-  [t b b]
-  [t f f]
-  [b t b]
-  [b b b]
-  [b f f]
-  [f t f]
-  [f b f]
-  [f f f])
-
-(define-truth-table ∨
-  [t t t]
-  [t b t]
-  [t f t]
-  [b t t]
-  [b b b]
-  [b f b]
-  [f t t]
-  [f b b]
-  [f f f])
-
-(define-truth-table ⇒
-  [t t t]
-  [t b b]
-  [t f f]
-  [b t t]
-  [b b b]
-  [b f f]
-  [f b t]
-  [f t t]
-  [f f t])
-
-(define-truth-table ⊃ ; material implication
-  [t t t]
-  [t b b]
-  [t f f]
-  [b t t]
-  [b b b]
-  [b f b]
-  [f t t]
-  [f b t]
-  [f f t])
-
-(define-truth-table ⇔
-  [t t t]
-  [t b b]
-  [t f f]
-  [b t b]
-  [b b b]
-  [b f f]
-  [f t f]
-  [f b f]
-  [f f t])
-
-(define-truth-table ≡ ; equivalence based on material implication
-  [t t t]
-  [t b b]
-  [t f f]
-  [b t b]
-  [b b b]
-  [b f b]
-  [f t f]
-  [f b b]
-  [f f t])
-
-(define-truth-table ¬
-  [t f]
-  [b b]
-  [f t])
-
-(define-truth-table □
-  [t t]
-  [b f]
-  [f f])
-
-(define-truth-table ◇
-  [t t]
-  [b t]
-  [f f])
-
-(define-truth-table B
-  [t f]
-  [b t]
-  [f f])
-
-;; Now we define a series of equivalences taken from the book
-
-;; We first define a macro to define and check an equivalence at once
-
-; We create the test module
-(module+ test
-  (require rackunit))
+(require rosette/lib/synthax
+         syntax/parse/define
+         "truth-tables.rkt"
+         (for-syntax
+           racket/syntax
+           syntax/to-string))
 
 ; we use 2 bits to represent a tvl value as a bitvector
 (define (bv-to-3 b)
@@ -153,139 +16,176 @@
     [(or (bveq b (bv #b01 2)) (bveq b (bv #b10 2))) 'b] ; #b01 and #b10 both represent 'b
     [(bveq b (bv #b00 2)) 'f])) ; #b00 is 'f
 
-(define-syntax-parser check-equivalence
-  [(_ (eq-to-check:id arg ...) lhs:expr rhs:expr)
+; this expands to code that returns a Rosette "solution"
+(define-syntax-parser verify-with-tvl-args
+  [(_ (f:id arg ...))
    (with-syntax
-     ([(arg/bv ...) ; symbols for symbolic variables
+     ([(arg/bv ...) ; symbols for tvl symbolic variables
        (for/list ([arg (syntax->list #'(arg ...))])
          (format-id #'eq-to-check "~a/bv" (syntax->datum arg)))])
-       #`(module+ test
-         (let () ; we use let to hide the following definitions from the outer scope
-           (define (eq-to-check arg ...) (eq? lhs rhs))
-           (define-symbolic arg/bv ... (bitvector 2))
-           (define arg (bv-to-3 arg/bv)) ...
-           (define result
-             (verify
-               (assert
-                 (eq-to-check arg ...))))
-           (define cex
-             (begin
-               (if (sat? result)
-                 (model result)
-                 #f)))
-           (define msg
+     #'(let () ; we use let to hide the following definitions from the outer scope
+         (define-symbolic arg/bv ... (bitvector 2))
+         (define arg (bv-to-3 arg/bv)) ...
+         (verify
+           (assert
+             (f arg ...)))))])
+
+(define-syntax-parser verify-is-true
+  [(_ (name:id arg ...) body)
+   #`(module+ test
+       (let () ; we use let to hide the following definitions from the outer scope
+         (define (name arg ...) body)
+         (define result
+           (verify-with-tvl-args (name arg ...)))
+         (define cex
+           (begin
              (if (sat? result)
-               (format
-                 #,(format "~a is falsified by the valuation ~~a" (syntax->string #'(eq-to-check)))
-                 cex)
-               #f))
-           (check-true (unsat? result) msg))))])
+               (model result)
+               #f)))
+         (define msg
+           (if (sat? result)
+             (format
+               #,(format "~a is falsified by the valuation ~~a" (syntax->string #'(name)))
+               cex)
+             #f))
+         (check-true (unsat? result) msg)))])
 
 ;; Figure 18.3
 
-; note that, below, using curly braces instead of parentheses enable infix syntax
-
-(check-equivalence (eq-18.3.1 p)
-    (¬ (¬ p))
-    p)
-
-(check-equivalence (eq-18.3.2 p q)
-    {p ∨ q}
-    (¬ {(¬ p) ∧ (¬ q)}))
-
-(check-equivalence (eq-18.3.3 p q)
-    {p ∧ q}
-    (¬ {(¬ p) ∨ (¬ q)}))
-
-(check-equivalence (eq-18.3.4 a b)
-    {a ⊃ b}
-    {(¬ a) ∨ b})
-
-(check-equivalence (eq-18.3.5 p)
-    {p ⊃ 'f}
-    (¬ p))
-
-(check-equivalence (eq-18.3.6.1 p q)
-    {p ≡ q}
-    {{p ⊃ q} ∧ {q ⊃ p}})
-(check-equivalence (eq-18.3.6.2 p q)
-    {p ≡ q}
-    {{(¬ p) ∨ q} ∧ {p ∨ (¬ q)}})
-
-(check-equivalence (eq-18.3.7.1 p q)
-    {p ⇒ q}
-    {(◇ p) ⊃ q})
-(check-equivalence (eq-18.3.7.2 p q)
-    {p ⇒ q}
-    {(□ (¬ p)) ∨ q})
-
-(check-equivalence (eq-18.3.8 p q)
-    {p ⇔ q}
-    {{p ⇒ q} ∧ {q ⇒ p}})
-
-(check-equivalence (eq-18.3.9.1 p)
-    (□ p)
-    (¬ (◇ (¬ p))))
-(check-equivalence (eq-18.3.9.2 p)
-    (□ p)
-    {(¬ p) ⇒ 'f})
-
-(check-equivalence (eq-18.3.10.1 p)
-    (◇ p)
-    (¬ (□ (¬ p))))
-(check-equivalence (eq-18.3.10.2 p)
-    (◇ p)
-    (¬ {p ⇒ 'f}))
-(check-equivalence (eq-18.3.10.3 p)
-    (◇ p)
-    {{p ⇒ 'f} ⇒ 'f})
-
-(check-equivalence (eq-18.3.11.1 p)
-    (B p)
-    (◇ {p ∧ (¬ p)}))
-(check-equivalence (eq-18.3.11.2 p)
-    (B p)
-    {(◇ p) ∧ (◇ (¬ p))})
-(check-equivalence (eq-18.3.11.3 p)
-    (B p)
-    {(◇ p) ∧ (¬ (□ p))})
-
-(check-equivalence (eq-18.3.12 p)
-    (□ (◇ p))
-    (◇ p))
-
-(check-equivalence (eq-18.3.13 p q)
-    (◇ {p ⇒ q})
-    {(◇ p) ⇒ (◇ q)})
-
-(check-equivalence (eq-18.3.13.1 p q)
-    (□ {p ∧ q})
-    {(□ p) ∧ (□ q)})
-(check-equivalence (eq-18.3.13.2 p q)
-    (□ {p ∨ q})
-    {(□ p) ∨ (□ q)})
-(check-equivalence (eq-18.3.13.3 p q)
-    (◇ {p ∧ q})
-    {(◇ p) ∧ (◇ q)})
-(check-equivalence (eq-18.3.13.4 p q)
-    (◇ {p ∨ q})
-    {(◇ p) ∨ (◇ q)})
-
-(check-equivalence (eq-19.2.7.1 p q)
-    {p ⊃ q}
-    {(¬ q) ⊃ (¬ p)})
-
-;; Now we check whether we can construct ⊃ from ⇒ and other modalities (see the discussion in Section 18.2.3) using an expression of fixed maximum depth
+; We will require the dimp-from-mimp-test module in the test module, so we need to declare it first:
+(module+ dimp-from-mimp-test
+  (require rackunit))
 
 (module+ test
+  ; We use the rackunit testing framework
+  (require rackunit))
+
+(verify-is-true (eq-18.3.1 p)
+    (eq?
+      (¬ (¬ p))
+      p))
+
+(verify-is-true (eq-18.3.2 p q)
+    (eq?
+      {p ∨ q}
+      (¬ {(¬ p) ∧ (¬ q)})))
+
+(verify-is-true (eq-18.3.3 p q)
+    (eq?
+      {p ∧ q}
+      (¬ {(¬ p) ∨ (¬ q)})))
+
+(verify-is-true (eq-18.3.4 a b)
+    (eq?
+      {a ⊃ b}
+      {(¬ a) ∨ b}))
+
+(verify-is-true (eq-18.3.5 p)
+    (eq?
+      {p ⊃ 'f}
+      (¬ p)))
+
+(verify-is-true (eq-18.3.6.1 p q)
+    (eq?
+      {p ≡ q}
+      {{p ⊃ q} ∧ {q ⊃ p}}))
+(verify-is-true (eq-18.3.6.2 p q)
+    (eq?
+      {p ≡ q}
+      {{(¬ p) ∨ q} ∧ {p ∨ (¬ q)}}))
+
+(verify-is-true (eq-18.3.7.1 p q)
+    (eq?
+      {p ⇒ q}
+      {(◇ p) ⊃ q}))
+(verify-is-true (eq-18.3.7.2 p q)
+    (eq?
+      {p ⇒ q}
+      {(□ (¬ p)) ∨ q}))
+
+(verify-is-true (eq-18.3.8 p q)
+    (eq?
+      {p ⇔ q}
+      {{p ⇒ q} ∧ {q ⇒ p}}))
+
+(verify-is-true (eq-18.3.9.1 p)
+    (eq?
+      (□ p)
+      (¬ (◇ (¬ p)))))
+(verify-is-true (eq-18.3.9.2 p)
+    (eq?
+      (□ p)
+      {(¬ p) ⇒ 'f}))
+
+(verify-is-true (eq-18.3.10.1 p)
+    (eq?
+      (◇ p)
+      (¬ (□ (¬ p)))))
+(verify-is-true (eq-18.3.10.2 p)
+    (eq?
+      (◇ p)
+      (¬ {p ⇒ 'f})))
+(verify-is-true (eq-18.3.10.3 p)
+    (eq?
+      (◇ p)
+      {{p ⇒ 'f} ⇒ 'f}))
+
+(verify-is-true (eq-18.3.11.1 p)
+    (eq?
+      (B p)
+      (◇ {p ∧ (¬ p)})))
+(verify-is-true (eq-18.3.11.2 p)
+    (eq?
+      (B p)
+      {(◇ p) ∧ (◇ (¬ p))}))
+(verify-is-true (eq-18.3.11.3 p)
+    (eq?
+      (B p)
+      {(◇ p) ∧ (¬ (□ p))}))
+
+(verify-is-true (eq-18.3.12 p)
+    (eq?
+      (□ (◇ p))
+      (◇ p)))
+
+(verify-is-true (eq-18.3.13 p q)
+    (eq?
+      (◇ {p ⇒ q})
+      {(◇ p) ⇒ (◇ q)}))
+
+(verify-is-true (eq-18.3.13.1 p q)
+    (eq?
+      (□ {p ∧ q})
+      {(□ p) ∧ (□ q)}))
+(verify-is-true (eq-18.3.13.2 p q)
+    (eq?
+      (□ {p ∨ q})
+      {(□ p) ∨ (□ q)}))
+(verify-is-true (eq-18.3.13.3 p q)
+    (eq?
+      (◇ {p ∧ q})
+      {(◇ p) ∧ (◇ q)}))
+(verify-is-true (eq-18.3.13.4 p q)
+    (eq?
+      (◇ {p ∨ q})
+      {(◇ p) ∨ (◇ q)}))
+
+(verify-is-true (eq-18.2.7.1 p q)
+    (eq?
+      {p ⊃ q}
+      {(¬ q) ⊃ (¬ p)}))
+
+;; Now we check whether we can construct ⊃ from ⇒ and other modalities using an expression of fixed maximum depth
+
+(module+ dimp-from-mimp-test
   (define-grammar (dimp-and-modalities p q)
-    [expr
-      (choose
-        p q 'f
-        {(expr) ⇒ (expr)}
-        (¬ (expr)) ; we also throw in negation, which does not seem to help
-        (◇ (expr))
-        (□ (expr)))])
+                  [expr
+                    (choose
+                      p q 'f
+                      {(expr) ⇒ (expr)}
+                      (¬ (expr)) ; we also throw in negation, which does not seem to help
+                      (◇ (expr))
+                      (□ (expr)))])
 
   (define (my-mimp p q)
     (dimp-and-modalities p q #:depth 4))
@@ -298,10 +198,13 @@
 
   (define-symbolic p/bv q/bv (bitvector 2))
 
-  (define my-mimp-solution
+  (define dimp-from-mimp-solution
     (synthesize
       #:forall (list p/bv q/bv)
       #:guarantee (check-is-mimp my-mimp (bv-to-3 p/bv) (bv-to-3 q/bv))))
 
   ;; We cannot construct ⊃ using expression generated by the grammar with depth at most 4:
-  (check-false (sat? my-mimp-solution)))
+  (check-false (sat? dimp-from-mimp-solution)))
+
+(module+ test
+  (require (submod ".." dimp-from-mimp-test)))
