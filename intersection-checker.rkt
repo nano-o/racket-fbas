e079246addc6365fd8b584572949eb65a4fc3a9d
Giuliano Losa <giuliano@losa.fr>
2024-01-13 21:04:14 -0800

    add support for nested qsets in intersection-checker

--- intersection-checker.rkt
+++ intersection-checker.rkt
@@ -1,29 +1,27 @@
 #lang rosette/safe
 
 (require
-  (only-in "qset.rkt" qset)
+  (only-in "qset2.rkt" qset)
   "tvl-verification.rkt"
   "truth-tables.rkt"
   syntax/parse/define
   (for-syntax
-    "qset.rkt"
+    "qset2.rkt"
     racket/syntax
     racket/pretty))
 
-; NOTE: for now we assume no inner qsets
-
 (define-syntax (check-intertwined stx)
   (syntax-parse stx
-    [(_ name:id wst:expr)
+    [(_ name:id qsets:expr)
      ; NOTE we need a separate module to pass to eval-syntax
      (define qset-map
-       (eval-syntax #'wst (module->namespace "qset.rkt")))
+       (flatten-qsets (eval-syntax #'qsets (module->namespace "qset2.rkt"))))
      ; collect all points
      (define points
        (dict-keys qset-map))
      ; now we need to create the formula
      (with-syntax
-       ([fmla-fn (format-id #'name "~a-intertwined" #'name)] ; symbol for the function we define
+       ([fmla-fn (format-id #'name "~a-intertwined?" #'name)] ; symbol for the function we define
         [(p ...) ; symbols for the tvl variables
          (for/list ([p points])
            (format-id #'() "~a" p))])
@@ -34,12 +32,12 @@
        (define (negate p)
          (with-syntax ([p p])
            #'(¬ p)))
-       ; TODO: the following only works if there are no inner quorumsets
+       ; TODO: the following only works if there are no inner quorumsets (it's okay since, above, we have flattened the qsets)
        (define (closedAx p polarity)
          (define q (dict-ref qset-map p))
-         (define n (length (qset-validators q)))
+         (define n (set-count (qset-validators q)))
          (define k (qset-threshold q))
-         (define bs (combinations (qset-validators q) (+ (- n k) 1))) ; blocking sets
+         (define bs (combinations (set->list (qset-validators q)) (+ (- n k) 1))) ; blocking sets
          (define (blocking-fmla b polarity)
            (for/fold
              ([acc #''t])
@@ -76,28 +74,56 @@
   (require rackunit)
   (check-true
     (sat? (check-intertwined semitopo-1
-      `((p . ,(qset 1 '(p) null))
-        (q . ,(qset 1 '(q) null))))))
+      `((p . ,(qset 1 (seteqv 'p) (set)))
+        (q . ,(qset 1 (seteqv 'q) (set)))))))
   (check-false
     (sat? (check-intertwined semitopo-2
-      `((p . ,(qset 1 '(q) null))
-        (q . ,(qset 1 '(q) null))))))
+      `((p . ,(qset 1 (seteqv 'q) (set)))
+        (q . ,(qset 1 (seteqv 'q) (set)))))))
   (check-false
     (sat? (check-intertwined semitopo-3
-      `((p . ,(qset 1 '(q) null))
-        (q . ,(qset 1 '(q) null))
-        (r . ,(qset 1 '(p) null))))))
+      `((p . ,(qset 1 (seteqv 'q) (set)))
+        (q . ,(qset 1 (seteqv 'q) (set)))
+        (r . ,(qset 1 (seteqv 'p) (set)))))))
   (check-true
     (sat? (check-intertwined semitopo-4
-      `((p . ,(qset 1 '(q) null))
-        (q . ,(qset 1 '(q) null))
-        (r . ,(qset 1 '(r) null))))))
+      `((p . ,(qset 1 (seteqv 'q) (set)))
+        (q . ,(qset 1 (seteqv 'q) (set)))
+        (r . ,(qset 1 (seteqv 'r) (set)))))))
   (check-true
     (sat? (check-intertwined semitopo-5 ; here we have a confused point
-      `((p . ,(qset 1 '(q) null))
-        (q . ,(qset 1 '(q) null))
-        (a . ,(qset 1 '(b) null))
-        (b . ,(qset 1 '(b) null))
-        (x . ,(qset 2 '(p a) null)))))))
+      `((p . ,(qset 1 (seteqv 'q) (set)))
+        (q . ,(qset 1 (seteqv 'q) (set)))
+        (a . ,(qset 1 (seteqv 'b) (set)))
+        (b . ,(qset 1 (seteqv 'b) (set)))
+        (x . ,(qset 2 (seteqv 'p 'a) (set)))))))
+  (check-false
+    (sat? (check-intertwined semitopo-6
+      `((p . ,(qset 1 (seteqv) (set (qset 2 (seteqv 1 2 3) (set)))))
+        (q . ,(qset 1 (seteqv) (set (qset 2 (seteqv 1 2 3) (set)))))
+        (1 . ,(qset 2 (seteqv 1 2 3) (set)))
+        (2 . ,(qset 2 (seteqv 1 2 3) (set)))
+        (3 . ,(qset 2 (seteqv 1 2 3) (set)))))))
+  (check-true
+    (sat? (check-intertwined semitopo-7
+      `((p . ,(qset 1 (seteqv) (set (qset 1 (seteqv 1 2 3) (set)))))
+        (q . ,(qset 1 (seteqv) (set (qset 1 (seteqv 1 2 3) (set)))))
+        (1 . ,(qset 1 (seteqv 1 2 3) (set)))
+        (2 . ,(qset 1 (seteqv 1 2 3) (set)))
+        (3 . ,(qset 1 (seteqv 1 2 3) (set)))))))
+  (check-false
+    (sat? (check-intertwined semitopo-8
+      `((p . ,(qset 1 (seteqv) (set (qset 1 (seteqv 1 2 3) (set)))))
+        (q . ,(qset 1 (seteqv) (set (qset 1 (seteqv 1 2 3) (set)))))
+        (1 . ,(qset 2 (seteqv 1 2 3) (set)))
+        (2 . ,(qset 2 (seteqv 1 2 3) (set)))
+        (3 . ,(qset 2 (seteqv 1 2 3) (set)))))))
+  (check-true
+    (sat? (check-intertwined semitopo-9
+      `((p . ,(qset 1 (seteqv) (set (qset 1 (seteqv 1 2 3) (set)))))
+        (q . ,(qset 1 (seteqv) (set (qset 1 (seteqv 1 2 3) (set)))))
+        (1 . ,(qset 2 (seteqv 1 2 3) (set)))
+        (2 . ,(qset 1 (seteqv 1 2 3) (set)))
+        (3 . ,(qset 2 (seteqv 1 2 3) (set))))))))
 
 ; TODO: pull data from stellarbeat
---
+++ qset2.rkt
@@ -0,0 +1,94 @@
+#lang racket
+(require
+  (only-in racket/hash hash-union!)
+  racket/trace)
+
+(provide
+  (struct-out qset)
+  flatten-qsets)
+
+(define-struct qset (threshold validators inner-qsets) #:prefab)
+
+(define (qset/kw #:threshold t #:validators vs #:inner-qsets qs)
+  (qset t vs qs))
+
+(module+ test
+  (require rackunit)
+  (define qset-1
+    (qset/kw #:threshold 2 #:validators (seteqv 1 2 3) #:inner-qsets (set)))
+  (define qset-2
+    (qset/kw #:threshold 2 #:validators (seteqv) #:inner-qsets (set)))
+  (define qset-3
+    (qset/kw #:threshold 2 #:validators (seteqv 'a 'b 'c) #:inner-qsets (set)))
+  (define qset-4
+    (qset/kw #:threshold 2 #:validators (seteqv 'x 'y 'z) #:inner-qsets (set)))
+  (define qset-5
+    (qset/kw #:threshold 2 #:validators (set) #:inner-qsets (set qset-1 qset-3 qset-4)))
+  (define qset-6
+    (qset/kw #:threshold 2 #:validators (seteqv 'A) #:inner-qsets (set qset-1 qset-3 qset-4)))
+
+  ; we now check that equal? on qsets behaves as we expect:
+  (check-true
+    (equal?
+      qset-1
+      (qset/kw #:threshold 2 #:validators (seteqv 2 1 3 1) #:inner-qsets (set))))
+  (check-true
+    (equal?
+      qset-6
+      (qset/kw
+        #:threshold 2
+        #:validators (seteqv 'A)
+        #:inner-qsets
+        (set
+          qset-4
+          qset-3
+          (qset/kw #:threshold 2 #:validators (seteqv 2 1 3 1) #:inner-qsets (set)))))))
+
+(define (inner-qsets-rec qset)
+  (define iqs
+    (qset-inner-qsets qset))
+  (set-union
+    iqs
+    (for/fold
+      ([acc (set)])
+      ([iq (in-set iqs)])
+      (set-union acc (inner-qsets-rec iq)))))
+
+(module+ test
+  (check-equal?
+    (qset-inner-qsets qset-6)
+    (set (qset 2 (seteqv 'z 'y 'x) (set)) (qset 2 (seteqv 'a 'b 'c) (set)) (qset 2 (seteqv 1 2 3) (set)))))
+
+(define (flatten-qsets qset-map)
+  ; builds a new qset configuration that has quorum-intersection if and only if the original one has it, and where no quorumset has any inner quorum sets.
+  ; this might be premature optimization...
+  (define (qset-symbol q)
+    (string->unreadable-symbol (~v (equal-hash-code q))))
+  (define new-qset-map
+    (make-hash qset-map)) ; we want it mutable
+  (trace-define (flatten q) ; NOTE: has side-effects on new-qset-map!
+    (define flattened-iqs
+      (for/set ([iq (in-set (qset-inner-qsets q))])
+        (flatten iq)))
+    (define new-points
+      (for/hash ([fiq flattened-iqs])
+        (values (qset-symbol fiq) fiq))) ; it's okay if it already exists in new-qset-map
+    (hash-union!
+      new-qset-map
+      new-points
+      #:combine/key (λ (k v1 v2) v1)) ; NOTE: side-effect!
+    (qset/kw ; we remove the inner qsets and add corresponding points
+      #:threshold (qset-threshold q)
+      #:validators (set-union (qset-validators q) (apply seteqv (dict-keys new-points)))
+      #:inner-qsets (set)))
+  (for ([(p qset) (in-dict qset-map)])
+    (hash-set! new-qset-map p (flatten qset))) ; NOTE: side-effect on new-qset-map!
+  ; finally, return an alist
+  (for/list ([(q qs) (in-dict new-qset-map)])
+    (cons q qs)))
+
+(module+ test
+  (check-true
+    (for/and ([q (dict-values (flatten-qsets `((p . ,qset-6))))])
+      (set-empty? (qset-inner-qsets q)))))
+
