30ec6827f7178a702bdd1997a094b0749473091f
Giuliano Losa <giuliano@losa.fr>
2024-01-26 14:35:00 -0800

    add runtime versions of the verification routines

--- tvl-verification.rkt
+++ tvl-verification.rkt
@@ -1,6 +1,6 @@
-; We use the rosette/safe language to enable symbolic execution
+; We use the rosette language to enable symbolic execution
 ; We use the sweet-exp adapter to enable infix syntax when we write logical formulas
-#lang sweet-exp rosette/safe
+#lang sweet-exp rosette
 
 (require
   syntax/parse/define
@@ -11,7 +11,8 @@
 (provide
   verify-with-tvl-args
   verify-valid/tvl
-  bv-to-3)
+  bv-to-3
+  verify-valid/at-runtime)
 
 ; we use 2 bits to represent a tvl value as a bitvector
 (define (bv-to-3 b)
@@ -19,6 +20,9 @@
     [(bveq b (bv #b11 2)) 't] ; #b11 is 't
     [(or (bveq b (bv #b01 2)) (bveq b (bv #b10 2))) 'b] ; #b01 and #b10 both represent 'b
     [(bveq b (bv #b00 2)) 'f])) ; #b00 is 'f
+
+(define-namespace-anchor anchor)
+(define ns (namespace-anchor->namespace anchor))
 
 ; given a function which accepts tvl values as argument and returns a Racket boolean truth value (i.e. #t or #r), verify that the function always returns #t
 ; this expands to code that returns a Rosette "solution"
@@ -33,6 +37,48 @@
          (verify
            (assert
              (f arg ...)))))])
+
+; version that's not a macro
+(define (verify-with-tvl-args/at-runtime vars eqn)
+  (define bv-vars
+    (for/list ([v vars]) (string->symbol (format "~a/bv" v))))
+  (define code
+    `(begin
+       (define-symbolic ,@bv-vars (bitvector 2))
+       (define-values
+         ,vars
+         (values ,@(for/list ([v bv-vars]) `(bv-to-3 ,v))))
+       (verify (assert ,eqn))))
+  (eval code ns))
+
+(define (verify-valid/at-runtime vars fmla)
+  (define bv-vars
+    (for/list ([v vars]) (string->symbol (format "~a/bv" v))))
+  (define code
+    `(begin
+       (define-symbolic ,@bv-vars (bitvector 2))
+       (define-values
+         ,vars
+         (values ,@(for/list ([v bv-vars]) `(bv-to-3 ,v))))
+       (define fmla ,fmla)
+       (verify (assert (|| (eq? fmla 't) (eq? fmla 'b))))))
+  ; (pretty-print code)
+  (eval code ns))
+
+(module+ test-runtime
+  (require rackunit)
+  (check-false
+    (sat?
+      (verify-with-tvl-args/at-runtime '(p) '(eq? (¬ (¬ p)) p))))
+  (check-true
+    (sat?
+      (verify-with-tvl-args/at-runtime '(p) '(eq? (¬ p) p))))
+  (check-false
+    (sat?
+      (verify-with-tvl-args/at-runtime '(a b)
+        '(eq?
+           (⊃ a b)
+           (∨ (¬ a) b))))))
 
 (module+ test
   (require rackunit)
@@ -64,7 +110,6 @@
 (define-syntax-parser verify-valid/tvl
   [(_ (f:id arg ...))
    #'(let ()
-       ; TODO: is it to reuse arg like that?
        (define (g arg ...)
          (designated-value (f arg ...)))
        (verify-with-tvl-args (g arg ...)))])
