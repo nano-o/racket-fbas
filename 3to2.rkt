6f8ad835630da1ba9da0cb289f6fec3857c6fe43
Giuliano Losa <giuliano@losa.fr>
2024-01-26 08:27:53 -0800

    WIP debugging 3to2

---
+++ 3to2.rkt
@@ -0,0 +1,99 @@
+#lang racket
+
+(require
+  racket/generator)
+
+(provide
+  valid?/3to2
+  debug) ; given a tvl formula, generates a boolean formula that is valid if and only if the original tvl formula is valid
+
+; The idea is that, for each sub-formula f, we create two boolean variables f- and f+ that encode the tvl truth value of the formula (i.e. 00 is 'f, 01 and 10 is 'b, and 11 is 't). Then we just apply the truth tables to express the truth value of a formula in terms of the truth value of its subformulas.
+
+; NOTE we better use rosette's &&, ||, etc. (and not `and`, `or`, etc) to get the classical logical operations
+
+(define debug (make-parameter #f))
+
+(define (is-f p)
+  `(&& (! ,(car p)) (! ,(cdr p))))
+(define (is-t p)
+  `(&& ,(car p) ,(cdr p)))
+(define (is-b p)
+  `(|| (&& (! ,(car p)) ,(cdr p)) (&& ,(car p) (! ,(cdr p)))))
+
+(define (encode-∧* p qs) ; encodes "p is the conjunction of all the qs"
+  (define one-f
+    `(|| ,@(map is-f qs)))
+  (define one-b
+    `(|| ,@(map is-b qs)))
+  `(||
+     (&& ,one-f ,(is-f p)) ; TODO: don't repeat not one-f
+     (&& (! ,one-f) ,one-b ,(is-b p))
+     (&& (! ,one-f) (! ,one-b)) ,(is-t p)))
+
+(define (encode-∨* p qs) ; encodes "p is the disjunction of all the qs"
+  (define one-t
+    `(|| ,@(map is-t qs)))
+  (define one-b
+    `(|| ,@(map is-b qs)))
+  `(||
+     (&& ,one-t ,(is-t p))
+     (&& (! ,one-t) ,one-b ,(is-b p))
+     (&& (! ,one-t) (! ,one-b) ,(is-f p))))
+
+(define (encode-≡* p qs) ; encodes "p is the equivalence of all the qs"
+  (define one-t-one-f
+    `(&& (|| ,@(map is-f qs)) (|| ,@(map is-t qs))))
+  (define one-b
+    `(|| ,@(map is-b qs)))
+  `(||
+     (&& ,one-t-one-f ,(is-f p))
+     (&& (! ,one-t-one-f) ,one-b ,(is-b p))
+     (&& (! ,one-t-one-f) (! ,one-b) ,(is-t p))))
+
+(define (encode-¬ p q)
+  `(||
+     (&& ,(is-f q) ,(is-t p))
+     (&& ,(is-b q) ,(is-b p))
+     (&& ,(is-t q) ,(is-f p)))) ; encodes "p is not q"
+
+; TODO: we should probably avoid creating boolean variables for formulas we have seen already
+; TODO: could be done tail recursive: create vars for subformulas, create constraint, pass vars to recursive call
+(define (valid?/3to2 fmla)
+  (define sym-gen
+    (generator ()
+      (let loop ([i 0])
+        (yield (string->unreadable-symbol (~a i)))
+        (loop (+ 1 i)))))
+  (define cs (mutable-set)) ; we'll collect the constraints here
+  (define vars (mutable-set)) ; we'll collect boolean variables here
+  (define (3to2-rec f)
+    ; first we generate symbols f+ and f-
+    (define f+-
+      (if (debug)
+        `(,(string->symbol (format "~a+" f)) . ,(string->symbol (format "~a-" f)))
+        (if (symbol? f)
+          `(,(string->symbol (format "~a+" f)) . ,(string->symbol (format "~a-" f)))
+          `(,(sym-gen) . ,(sym-gen)))))
+    (set-add! vars (car f+-))
+    (set-add! vars (cdr f+-))
+    (define constraint
+      (match f
+        [`(∧* ,q ...)
+          (encode-∧* f+- (map 3to2-rec q))]
+        [`(∨* ,q ...)
+          (encode-∨* f+- (map 3to2-rec q))]
+        [`(≡* ,q ...)
+          (encode-≡* f+- (map 3to2-rec q))]
+        [`(¬ ,q)
+          (encode-¬ f+- (3to2-rec q))]
+        [(? symbol?) #t]))
+    (set-add! cs constraint)
+    f+-)
+  (define p (3to2-rec fmla))
+  ; finally, return the variables and the constraint
+  (define constraint
+    `(=> (&& ,@(set->list cs)) (|| ,(is-t p) ,(is-b p))))
+  ; (pretty-print constraint)
+  `(,vars . ,constraint))
+
+; (pretty-print (valid?/3to2 '(∧* a b)))
---
+++ test-3to2.rkt
@@ -0,0 +1,48 @@
+#lang sweet-exp racket
+
+(require
+  "3to2.rkt"
+  racket/syntax
+  rosette)
+
+(define (verify/syntax fmla)
+  (match-define `(,vars . ,constraint)
+    (valid?/3to2 fmla))
+  (with-syntax
+    ([(v ...) (for/list ([v vars]) (format-id #'() "~a" v))])
+    #`(begin
+        (define-symbolic v ... boolean?)
+      #,(datum->syntax #'() constraint))))
+
+(define (verify/datum fmla)
+  (match-define `(,vars . ,constraint)
+    (parameterize ([debug #t]) (valid?/3to2 fmla)))
+  `(begin
+     (define-symbolic ,@(set->list vars) boolean?)
+     (verify (assert ,constraint))))
+
+(define (verify-solver/datum fmla)
+  (match-define `(,vars . ,constraint)
+    (parameterize ([debug #t]) (valid?/3to2 fmla)))
+  `(begin
+     (define-symbolic ,@(set->list vars) boolean?)
+     (define solver (current-solver))
+     (solver-assert solver (list (! ,constraint)))
+     (solver-check solver)))
+
+(define test-fmla-1 '(≡* p (¬ (¬ p))))
+(define test-fmla-2 '(≡* (∨* p q) (¬ (∧* (¬ p) (¬ q)))))
+(define test-fmla-3 '(≡* (∧* p q) (¬ (∨* (¬ p) (¬ q)))))
+#;(with-output-to-file #:exists 'replace "debug.txt"
+  (thunk
+    (pretty-write
+      (model
+        (eval
+          (verify/datum test-fmla-1)
+          (module->namespace 'rosette))))))
+(with-output-to-file #:exists 'replace "debug.txt"
+  (thunk
+    (pretty-write
+        (eval
+          (verify-solver/datum test-fmla-2)
+          (module->namespace 'rosette)))))
