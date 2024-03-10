03461527fb01e9afb513f315edad13bff597286a
Giuliano Losa <giuliano@losa.fr>
2024-03-09 22:20:54 -0800

    simplify 3to2 using eval

--- 3to2.rkt
+++ 3to2.rkt
@@ -3,35 +3,23 @@
 (require
   "truth-tables.rkt"
   (only-in sugar define/caching)
-  racket/trace
-  racket/generator
   racket/match)
 
 (provide
   3to2
   t-or-b?
-  equiv-fmlas?
-  debug)
-
-; (begin-for-syntax
-  ; (println uops))
-; (define-syntax test (λ (_) (car uops)))
-; ((test) 't)
+  equiv-fmlas?)
 
  ; given a tvl formula, generates a boolean formula that is valid if and only if the original tvl formula is valid
 
 ; The idea is that, for each sub-formula f, we create two boolean variables f- and f+ that encode the tvl truth value of the formula (i.e. 00 is 'f, 01 and 10 is 'b, and 11 is 't). Then we just apply the truth tables to express the truth value of a formula in terms of the truth value of its subformulas.
-; TODO if we go with the terminology f- and f+ then we should have 10 is f, 01 is t, and 11/00 is b
 
-; NOTE we better use rosette's &&, ||, etc. (and not `and`, `or`, etc) to get the classical logical operations
-
-(define debug (make-parameter #f))
-
-(define (is-f p)
+; We encode 3vl values as pairs of booleans
+(define (is-f p) ; f is 10
   `(&& ,(car p) (! ,(cdr p))))
-(define (is-t p)
+(define (is-t p) ; t is 01
   `(&& (! ,(car p)) ,(cdr p)))
-(define (is-b p)
+(define (is-b p) ; b is 00 or 11
   `(|| (&& (! ,(car p)) (! ,(cdr p))) (&& ,(car p) ,(cdr p))))
 
 (module+ test
@@ -40,7 +28,7 @@
     (is-f '(p . q))
     '(&& p (! q))))
 
-; returns a function that checks whether its argument (a pair) is the 3vl value v
+; returns a function that checks whether its encoded 3vl argument (a pair) corresponds to the 3vl value v
 (define (is-tv v)
   (case v
     [(t) is-t]
@@ -52,210 +40,79 @@
     ((is-tv 'f) '(p . q))
     '(&& p (! q))))
 
-; TODO macro based on trugh tables...
-(define (encode-¬ p q)
-  `(||
-     (&& ,(is-f q) ,(is-t p))
-     (&& ,(is-b q) ,(is-b p))
-     (&& ,(is-t q) ,(is-f p)))) ; encodes "p is not q"
-
-(define (encode-□ p q)
-  `(||
-     (&& ,(is-f q) ,(is-f p))
-     (&& ,(is-b q) ,(is-f p))
-     (&& ,(is-t q) ,(is-t p))))
-
-(define (encode-◇ p q)
-  `(||
-     (&& ,(is-f q) ,(is-f p))
-     (&& ,(is-b q) ,(is-t p))
-     (&& ,(is-t q) ,(is-t p))))
-
-(define (encode-B p q)
-  `(||
-     (&& ,(is-f q) ,(is-f p))
-     (&& ,(is-b q) ,(is-t p))
-     (&& ,(is-t q) ,(is-f p))))
-
-; TODO macro
-(define (encode-∧ p q1 q2) ; encodes "p is the conjunction of q1 and q2"
-  ; `(||
-     ; (&& (|| ,((is-tv 'f) q1) ,((is-tv 'f) q2)) ,((is-tv 'f) p))
-     ; (&& (! (|| ,((is-tv 'f) q1) ,((is-tv 'f) q2))) (|| ,((is-tv 'b) q1) ,((is-tv 'b) q2)) ,((is-tv 'b) p))
-     ; (&& ,((is-tv 't) q1) ,((is-tv 't) q2) ,((is-tv 't) p))))
-  `(|| ,@(for*/list ([v1 truth-values]
-                     [v2 truth-values])
-           `(&& ,((is-tv v1) q1) ,((is-tv v2) q2) ,((is-tv (∧ v1 v2)) p)))))
-(define (encode-∨ p q1 q2)
-  ; `(||
-     ; (&& (|| ,((is-tv 't) q1) ,((is-tv 't) q2)) ,((is-tv 't) p))
-     ; (&& (! (|| ,((is-tv 't) q1) ,((is-tv 't) q2))) (|| ,((is-tv 'b) q1) ,((is-tv 'b) q2)) ,((is-tv 'b) p))
-     ; (&& ,((is-tv 'f) q1) ,((is-tv 'f) q2) ,((is-tv 'f) p))))
-  `(|| ,@(for*/list ([v1 truth-values]
-                     [v2 truth-values])
-           `(&& ,((is-tv v1) q1) ,((is-tv v2) q2) ,((is-tv (∨ v1 v2)) p)))))
-(define (encode-⇒ p q1 q2)
-  `(|| ,@(for*/list ([v1 truth-values]
-                     [v2 truth-values])
-           `(&& ,((is-tv v1) q1) ,((is-tv v2) q2) ,((is-tv (⇒ v1 v2)) p)))))
-(define (encode-≡ p q1 q2)
-  `(|| ,@(for*/list ([v1 truth-values]
-                     [v2 truth-values])
-           `(&& ,((is-tv v1) q1) ,((is-tv v2) q2) ,((is-tv (≡ v1 v2)) p)))))
-(define (encode-⊃ p q1 q2)
-  `(|| ,@(for*/list ([v1 truth-values]
-                     [v2 truth-values])
-           `(&& ,((is-tv v1) q1) ,((is-tv v2) q2) ,((is-tv (⊃ v1 v2)) p)))))
-(define (encode-⇔ p q1 q2)
-  `(|| ,@(for*/list ([v1 truth-values]
-                     [v2 truth-values])
-           `(&& ,((is-tv v1) q1) ,((is-tv v2) q2) ,((is-tv (⇔ v1 v2)) p)))))
-
-#;(define (rewrite-big-ops f)
-  ; TODO there seems to be a problem (disagreement with other metdhos on almost_sym_13)
-  ; rewrite the * ops
-  ; looks like something for nanopass
-  (match f
-    [`(∧ ,q1 ,q2)
-      `(∧ ,(rewrite-big-ops q1) ,(rewrite-big-ops q2))]
-    [`(∨ ,q1 ,q2)
-      `(∨ ,(rewrite-big-ops q1) ,(rewrite-big-ops q2))]
-    [`(⇒ ,q1 ,q2)
-      `(⇒ ,(rewrite-big-ops q1) ,(rewrite-big-ops q2))]
-    [`(≡ ,q1 ,q2)
-      `(≡ ,(rewrite-big-ops q1) ,(rewrite-big-ops q2))]
-    [`(⇔ ,q1 ,q2)
-      `(⇔ ,(rewrite-big-ops q1) ,(rewrite-big-ops q2))]
-    [`(⊃ ,q1 ,q2)
-      `(⊃ ,(rewrite-big-ops q1) ,(rewrite-big-ops q2))]
-    [`(□ ,q)
-      `(□ ,(rewrite-big-ops q))]
-    [`(◇ ,q)
-      `(◇ ,(rewrite-big-ops q))]
-    [`(¬ ,q)
-      `(¬ ,(rewrite-big-ops q))]
-    [`(B ,q)
-      `(B ,(rewrite-big-ops q))]
-    [(and sym (? symbol?)) sym]
-    [`(∧* ,q ...)
-      (match q
-        [`(,qs ...) #:when (> (length qs) 2)
-                    `(∧ ,(rewrite-big-ops (car qs)) ,(rewrite-big-ops `(∧* ,@(cdr qs))))]
-        [`(,q1 ,q2) `(∧ ,(rewrite-big-ops q1) ,(rewrite-big-ops q2))]
-        [`(,q1) (rewrite-big-ops q1)])]
-        ['() #t]
-    [`(∨* ,q ...)
-      (match q
-        [`(,qs ...) #:when (> (length qs) 2)
-                    `(∨ ,(rewrite-big-ops (car qs)) ,(rewrite-big-ops `(∨* ,@(cdr qs))))]
-        [`(,q1 ,q2) `(∨ ,(rewrite-big-ops q1) ,(rewrite-big-ops q2))]
-        [`(,q1) (rewrite-big-ops q1)])]
-        ['() #f]
-    [`(≡* ,q ...)
-      (match q
-        [`(,qs ...) #:when (> (length qs) 2)
-                    `(≡ ,(rewrite-big-ops (car qs)) ,(rewrite-big-ops `(≡* ,@(cdr qs))))]
-        [`(,q1 ,q2) `(≡ ,(rewrite-big-ops q1) ,(rewrite-big-ops q2))]
-        [`(,q1) (rewrite-big-ops q1)])]
-        ['() (raise (error "empty equivalence is not supported"))] ; TODO: what should this be?
-    [x (error (format "unhandled case: ~a" x))]))
-
-(define (encode-∧* p qs) ; encodes "p is the conjunction of all the qs"
-  (define one-f
-    `(|| ,@(map is-f qs)))
-  (define one-b
-    `(|| ,@(map is-b qs)))
-  `(||
-     (&& ,one-f ,(is-f p))
-     (&& (! ,one-f) ,one-b ,(is-b p))
-     (&& (! ,one-f) (! ,one-b) ,(is-t p))))
-     ; (&& (! ,one-f)
-         ; (|| (&& ,one-b ,(is-b p))
-             ; (&& (! ,one-b) ,(is-t p))))))
-
-(define (encode-∨* p qs) ; encodes "p is the disjunction of all the qs"
-  (define one-t
-    `(|| ,@(map is-t qs)))
-  (define one-b
-    `(|| ,@(map is-b qs)))
-  `(||
-     (&& ,one-t ,(is-t p))
-     (&& (! ,one-t) ,one-b ,(is-b p))
-     (&& (! ,one-t) (! ,one-b) ,(is-f p))))
-     ; (&& (! ,one-t)
-         ; (|| (&& ,one-b ,(is-b p))
-             ; (&& (! ,one-b) ,(is-f p))))))
-
-(define (encode-≡* p qs) ; encodes "p is the equivalence of all the qs"
-  (define one-t-one-f
-    `(&& (|| ,@(map is-f qs)) (|| ,@(map is-t qs))))
-  (define one-b
-    `(|| ,@(map is-b qs)))
-  `(||
-     (&& ,one-t-one-f ,(is-f p))
-     (&& (! ,one-t-one-f) ,one-b ,(is-b p))
-     (&& (! ,one-t-one-f) (! ,one-b) ,(is-t p))))
-     ; (&& (! ,one-t-one-f)
-         ; (|| (&& ,one-b ,(is-b p))
-             ; (&& (! ,one-b) ,(is-t p))))))
-
-; TODO: could be done tail recursive: create vars for subformulas, create constraint, pass vars to recursive call
-; TODO: use memoize, or use make-caching-prog or define/caching in sugar
 (define (3to2 fmla)
+  ; TODO make functional?
   (define cs (mutable-set)) ; we'll collect the constraints here
   (define vars (mutable-set)) ; we'll collect boolean variables here TODO why?
+  (define (truth-tables-eval op)
+    (eval op (module->namespace "truth-tables.rkt"))) ; TODO would using a macro be more performant?
   (define/caching (3to2-rec f)
     ; Here we want to create a 3vl variable for f and relate it the the 3vl variable corresponding to its constituant subformulas
     ; We'll return the two 3vl variable f- and f+ and the constraint
     (define f-+
       (cond
-        [(debug)
-         (cons (string->symbol (format "~a-" f)) (string->symbol (format "~a+" f)))]
         [(symbol? f) ; f is a variable; it has no subformulas
          (cons (string->symbol (format "~a-" f)) (string->symbol (format "~a+" f)))]
         [else (cons (gensym) (gensym))]))
     (set-add! vars (car f-+))
     (set-add! vars (cdr f-+))
-    (define (process-sub sub-f)
-        (3to2-rec sub-f))
     (define constraint
-      (match f ; TODO macro
+      (match f
         [(? symbol?) #:when (member f truth-values) ((is-tv f) f-+)]
         [(? symbol?) #t]
-        [`(∧ ,q1 ,q2)
-          (encode-∧ f-+ (process-sub q1) (process-sub q2))]
-        [`(∨ ,q1 ,q2)
-          (encode-∨ f-+ (process-sub q1) (process-sub q2))]
-        [`(⊃ ,q1 ,q2)
-          (encode-⊃ f-+ (process-sub q1) (process-sub q2))]
-        [`(⇒ ,q1 ,q2)
-          (encode-⇒ f-+ (process-sub q1) (process-sub q2))]
-        [`(≡ ,q1 ,q2)
-          (encode-≡ f-+ (process-sub q1) (process-sub q2))]
-        [`(⇔ ,q1 ,q2)
-          (encode-⇔ f-+ (process-sub q1) (process-sub q2))]
-        [`(◇ ,q)
-          (encode-◇ f-+ (process-sub q))]
-        [`(□ ,q)
-          (encode-□ f-+ (process-sub q))]
-        [`(¬ ,q)
-          (encode-¬ f-+ (process-sub q))]
-        [`(B ,q)
-          (encode-B f-+ (process-sub q))]
-        [`(∧* ,q ...)
-          (encode-∧* f-+ (map process-sub q))]
-        [`(∨* ,q ...)
-          (encode-∨* f-+ (map process-sub q))]
-        [`(≡* ,q ...)
-          (encode-≡* f-+ (map process-sub q))]))
+        [`(,uop ,subf)
+          (define subf-+ (3to2-rec subf))
+          `(||
+            ,@(for/list ([v truth-values])
+                `(&& ,((is-tv v) subf-+) ,((is-tv ((truth-tables-eval uop) v)) f-+))))]
+        [`(,bop ,subf1 ,subf2)
+          (define subf1-+ (3to2-rec subf1))
+          (define subf2-+ (3to2-rec subf2))
+          `(||
+            ,@(for*/list ([v1 truth-values]
+                         [v2 truth-values])
+                `(&& ,((is-tv v1) subf1-+) ,((is-tv v2) subf2-+) ,((is-tv ((truth-tables-eval bop) v1 v2)) f-+))))]
+        ; We do something a bit ad-hoc for the * operations
+        ; TODO does it have any advantage over translating to bops?
+        [`(∧* ,subf ...)
+          (define subf-+ (map 3to2-rec subf))
+          (define one-f
+            `(|| ,@(map is-f subf-+)))
+          (define one-b
+            `(|| ,@(map is-b subf-+)))
+          `(||
+             (&& ,one-f ,(is-f f-+))
+             (&& (! ,one-f) ,one-b ,(is-b f-+))
+             (&& (! ,one-f) (! ,one-b) ,(is-t f-+)))]
+        [`(∨* ,subf ...)
+          (define subf-+ (map 3to2-rec subf))
+          (define one-t
+            `(|| ,@(map is-t subf-+)))
+          (define one-b
+            `(|| ,@(map is-b subf-+)))
+          `(||
+             (&& ,one-t ,(is-t f-+))
+             (&& (! ,one-t) ,one-b ,(is-b f-+))
+             (&& (! ,one-t) (! ,one-b) ,(is-f f-+)))]
+        [`(≡* ,subf ...)
+          (define subf-+ (map 3to2-rec subf))
+          (define one-t-one-f
+            `(&& (|| ,@(map is-f subf-+)) (|| ,@(map is-t subf-+))))
+          (define one-b
+            `(|| ,@(map is-b subf-+)))
+          `(||
+             (&& ,one-t-one-f ,(is-f f-+))
+             (&& (! ,one-t-one-f) ,one-b ,(is-b f-+))
+             (&& (! ,one-t-one-f) (! ,one-b) ,(is-t f-+)))]))
     (set-add! cs constraint)
     f-+)
-  ; (define p (3to2-rec (rewrite-big-ops fmla)))
   (define p (3to2-rec fmla))
   (define constraint
     `(&& ,@(set->list cs)))
-  ; (println "finished generating fmla")
   `(,p ,vars . ,constraint))
+
+(module+ test
+  (3to2 '(B p)))
 
 (define (t-or-b? fmla)
   (match-define `(,p ,vars . ,c) (3to2 fmla))
